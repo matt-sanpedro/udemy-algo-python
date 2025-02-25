4468424fe48c1795c102506ef3d52c88e05d0ca7
matt-sanpedro <mattsanpedro7@gmail.com>
2025-02-24 21:15:06 -0800

    Added example on if statments with user input

---
+++ section-00-python-refresh/p-06-if-statement.py
@@ -0,0 +1,10 @@
+day_of_week = input("What day of the week is it? ").lower()
+
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("I'll pay you 50 cents on Tuesday for a hamburger today.")
+else:
+    print("Full speed ahead!")
+
+print("This always runs")
