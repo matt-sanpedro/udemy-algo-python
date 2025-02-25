0f1f21dfd68f7b33b5676dda7d741fc3af5fd0dc
matt-sanpedro <mattsanpedro7@gmail.com>
2025-02-24 21:21:31 -0800

    Added example on using in keyword with a set

---
+++ section-00-python-refresh/p-07-in-keyword.py
@@ -0,0 +1,8 @@
+# in: keyword to check if value is a member in a list, tuple, or set
+friends = ["Rolf", "Sarah", "Ruth"]
+print("Ruth" in friends)
+
+movies_watched = {"the matrix", "green book", "titanic"}
+user_movie = input("Enter a movie you've watched recently: ").lower()
+
+print(user_movie in movies_watched)
