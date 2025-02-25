a244082b767b85d129fa9faa8b9e151f43dd0f2f
matt-sanpedro <mattsanpedro7@gmail.com>
2025-02-24 21:12:04 -0800

    Added examples on boolean operators

---
+++ section-00-python-refresh/p-05-booleans.py
@@ -0,0 +1,16 @@
+# checks for equality
+print(5 == 5)
+
+# greater/less than
+print(5 > 5)
+
+# not equals operator
+print(10 != 10)
+
+# can also compare lists
+friends = ["Rolf", "Sarah", "Ruth"]
+abroad = ["Rolf", "Sarah", "Ruth"]
+print(friends == abroad)
+
+# is: keyword to compare if the two variables are the same object in memory
+print(friends is abroad)
