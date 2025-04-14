33d527b742903f73c48d1a14f2ef4d18fdaee3bf
matt-sanpedro <mattsanpedro7@gmail.com>
2025-04-13 20:51:19 -0700

    Added example on importing with the use of __name__

---
+++ section-00-python-refresh/p-28-importing/mymodule.py
@@ -0,0 +1,7 @@
+def divide(dividend, divisor):
+    return dividend / divisor
+
+# __name__ is a special variable in Python that is set to the name of the module when it is imported.
+print("mymodule.py ", __name__)
+
+import libs.mylib
