c3e298d0f3a63612f8a68c65c9a6d1cae4224f6e
matt-sanpedro <mattsanpedro7@gmail.com>
2025-04-14 21:53:56 -0700

    Added examples of decorators with parameters resulting in three levels of functions

---
+++ section-00-python-refresh/p-36-decorators-with-parameters.py
@@ -0,0 +1,35 @@
+import functools
+
+# simple decorators allow the modification of functions or methods
+user = {"username": "john", "access_level": "guest"}
+
+# make_secure is now a factory function that takes an argument
+def make_secure(access_level):
+    # make_secure is a decorator that takes a function as an argument
+    def decorator(func):
+        # secure_function is a wrapper for func
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}"
+            
+        return secure_function
+    return decorator
+
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 1234"
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+print(get_admin_password())
+print(get_dashboard_password())
+
+user = {"username": "anna", "access_level": "admin"}
+
+print(get_admin_password())
+print(get_dashboard_password())
