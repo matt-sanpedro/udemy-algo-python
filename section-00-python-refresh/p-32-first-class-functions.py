b79230108ad323434f6a05d41bfdea1abf1c4a4c
matt-sanpedro <mattsanpedro7@gmail.com>
2025-04-14 20:58:14 -0700

    Added search and operator examples of first class function

---
+++ section-00-python-refresh/p-32-first-class-functions.py
@@ -0,0 +1,36 @@
+from operator import itemgetter
+
+# first class function: function that behaves like a variable
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+    
+    return dividend / divisor
+
+def calculate(*values, operator):
+    return operator(*values)
+
+# exmaple use of first class function
+result = calculate(20, 4, operator=divide)
+print(result)
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}.")
+
+friends = [
+    {"name": "Alice", "age": 30},
+    {"name": "Bob", "age": 25},
+    {"name": "Charlie", "age": 35},
+    {"name": "David", "age": 28},
+    {"name": "Eve", "age": 22},
+]
+
+def get_friend_name(friend):
+    return friend["name"]
+
+print(search(friends, "Alice", get_friend_name))
+print(search(friends, "Alice", lambda friend: friend["name"]))
+print(search(friends, "Alice", itemgetter("name")))
