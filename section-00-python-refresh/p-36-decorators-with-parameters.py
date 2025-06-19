c3577d94576410de892b3efac64209c4a2e42719
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-18 23:45:01 -0700

    Added more comments and a bonus project idea

--- section-00-python-refresh/p-36-decorators-with-parameters.py
+++ section-00-python-refresh/p-36-decorators-with-parameters.py
@@ -1,11 +1,13 @@
 import functools
 
+# BONUS: refactor the code to take in the user as an argument to the decorator
+
 # simple decorators allow the modification of functions or methods
 user = {"username": "john", "access_level": "guest"}
 
-# make_secure is now a factory function that takes an argument
+# make_secure is now a factory function that takes an argument and returns a decorator
 def make_secure(access_level):
-    # make_secure is a decorator that takes a function as an argument
+    # decorator takes a function as an argument
     def decorator(func):
         # secure_function is a wrapper for func
         @functools.wraps(func)
