c1703c1185414abaf2e020165e2c668badc8b2b2
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-18 23:36:47 -0700

    Added print statements at several places in the make_secure decorator

--- section-00-python-refresh/p-35-decorator-func-parameters.py
+++ section-00-python-refresh/p-35-decorator-func-parameters.py
@@ -6,8 +6,12 @@ user = {"username": "john", "access_level": "admin"}
 # make_secure is a decorator that takes a function as an argument
 def make_secure(func):
     # secure_function is a wrapper for func
+    print(f"Decorating function [make_secure]: {func.__name__}")
+    # print(f"Decorating function: {func.__code__.co_name}")
     @functools.wraps(func)
     def secure_function(*args, **kwargs):
+        print(f"Decorating function [secure_function]: {func.__name__}")
+        # print(f"Decorating function: {func}")
         if user["access_level"] == "admin":
             return func(*args, **kwargs)
         else:
@@ -22,5 +26,7 @@ def get_password(panel):
     elif panel == "billing":
         return "super_secure_password"
 
+# get password is replaced by secure_function
+# print(get_password.__name__)
 print(get_password("admin"))
 print(get_password("billing"))
