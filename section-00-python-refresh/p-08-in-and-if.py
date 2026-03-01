d0bd824592184df83fc6fa81de939454e8915525
matt-sanpedro <mattsanpedro7@gmail.com>
2026-02-28 16:14:53 -0800

    Fixed typo in print statement

--- section-00-python-refresh/p-08-in-and-if.py
+++ section-00-python-refresh/p-08-in-and-if.py
@@ -2,6 +2,6 @@ movies_watched = {"The Matrix", "Inception", "Interstellar", "The Dark Knight"}
 user_movie = input("Enter a movie name: ")
 
 if user_movie in movies_watched:
-    print(f"I've wathced {user_movie} too!")
+    print(f"I've watched {user_movie} too!")
 else:
     print("I haven't watched that movie yet.")
