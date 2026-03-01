b41b4002c353df71924cc913f77c1dd5019587c6
matt-sanpedro <mattsanpedro7@gmail.com>
2026-02-28 18:50:19 -0800

    Added comments to each block in the try logic

--- section-00-python-refresh/p-30-error-handling.py
+++ section-00-python-refresh/p-30-error-handling.py
@@ -9,12 +9,16 @@ grades = []
 
 print("Welcome to the average grade program.")
 
+# put code that might fail here
 try:
     average = divide(sum(grades), len(grades))
+# runs only if ZeroDivisionError is raised
 except ZeroDivisionError as e:
     print(f"There are no grades to average. Error: {e}")
+# runs only if the TRY block raises an exception that is not caught by the previous except block(s)
 else:
     print(f"The average grade is {average}.")
+# runs no matter what, even if an exception is raised and not caught
 finally:
     # run this code no matter what
     print("Thank you")
